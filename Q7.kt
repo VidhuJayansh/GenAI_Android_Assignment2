a26958aa17efff04b813eba01d6f8f216cfa4e79
Vidhu Jayansh <20.jayanshvidhu@gmail.com>
2026-06-11 23:51:13 +0530

    Q7.kt

---
+++ Q7.kt
@@ -0,0 +1,13 @@
+data class User(
+    val id: Int,
+    val username: String,
+    val isActive: Boolean
+)
+
+fun main() {
+    val user1 = User(1, "Jyoti", true)
+    val user2 = user1.copy(isActive = false)
+
+    println(user1)
+    println(user2)
+}
