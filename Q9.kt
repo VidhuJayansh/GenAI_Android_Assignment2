aaff1b0aec9e4888faa43f25cde1f6d27a6da52a
Vidhu Jayansh <20.jayanshvidhu@gmail.com>
2026-06-11 23:51:58 +0530

    Q9.kt

---
+++ Q9.kt
@@ -0,0 +1,16 @@
+class IDGenerator {
+    companion object {
+        var currentId = 0
+
+        fun getNextId(): Int {
+            currentId++
+            return currentId
+        }
+    }
+}
+
+fun main() {
+    println(IDGenerator.getNextId())
+    println(IDGenerator.getNextId())
+    println(IDGenerator.getNextId())
+}
