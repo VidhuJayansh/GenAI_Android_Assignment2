b44cfa338d35d7460f337ca4bdf2ef4cf28329b7
Vidhu Jayansh <20.jayanshvidhu@gmail.com>
2026-06-11 23:49:47 +0530

    Q3.kt

---
+++ Q3.kt
@@ -0,0 +1,16 @@
+open class Vehicle {
+    open fun startEngine() {
+        println("Engine Started")
+    }
+}
+
+class Motorcycle : Vehicle() {
+    override fun startEngine() {
+        println("Vroom!")
+    }
+}
+
+fun main() {
+    val bike = Motorcycle()
+    bike.startEngine()
+}
