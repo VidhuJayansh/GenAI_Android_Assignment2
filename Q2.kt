6ab93876f8965490a400cc309cbd7ff651fbd8d5
Vidhu Jayansh <20.jayanshvidhu@gmail.com>
2026-06-11 23:49:16 +0530

    Q2.kt

---
+++ Q2.kt
@@ -0,0 +1,14 @@
+class Thermostat {
+    var temperature: Double = 0.0
+        private set
+
+    fun setTemperature(value: Double) {
+        temperature = value
+    }
+}
+
+fun main() {
+    val thermostat = Thermostat()
+    thermostat.setTemperature(25.5)
+    println(thermostat.temperature)
+}
